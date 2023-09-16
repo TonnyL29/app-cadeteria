0a2201a3bdfe06bd4f86591824f8f481c9f046b6
Jose <82624339+TonnyL29@users.noreply.github.com>
2023-09-16 16:20:17 -0300

    Modificacion del proyecto
    Se agregaron 2 componentes y se organizaron los elementos.

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+
+
+    <div className="conteiner">
+        <div className="row">
+
+            <div className="col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-6 col-xxl-4">
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+
+}
+
+export default Footer;
